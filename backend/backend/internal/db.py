efc4387d4490a38ffcaefc03bdd722a8cd466d24
Bryson Wu <brysonwu@student.ubc.ca>
2024-03-24 03:48:38 -0700

    restart with streamlit

--- backend/backend/internal/db.py
+++
@@ -1,7 +0,0 @@
-from backend.internal import secrets
-
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient(secrets.MONGODB_URI)
-cluster = client.Cluster0
-articles = cluster.articles
