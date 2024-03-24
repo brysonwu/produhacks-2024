60ba2789cf4c6308a6d26582860deb9fb76f16f9
lindama <lindima200@gmail.com>
2024-03-24 05:53:07 -0700

    init chat feature

--- preview/preview/pages/1_chat.py
+++ preview/preview/pages/1_chat.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import cohere
 
-co = cohere.Client(COHERE_API_KEY)
+from cohere import Client, ChatConnector
+
+# from preview.internal.secrets import COHERE_API_KEY
+
+co = Client(COHERE_API_KEY)
 
 st.title("PReview")
 
@@ -17,8 +21,45 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-# response = f"Echo: {prompt}"
-    response = co.chat(message=prompt).text
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    response = co.chat_stream(
+        model="command-r",
+        message=prompt,
+        connectors=[ChatConnector(id='web-search')]
+    )
+
+    print('\nChatbot:')
+
+    citations = []
+    cited_documents = []
+    cited_document_titles = {}
+    for event in response:
+        if event.event_type == 'citation-generation':
+            citations.extend(event.citations)
+        elif event.event_type == 'search-results':
+            cited_documents = event.documents
+
+        if citations:
+
+            # print("\nDOCUMENTS:")
+            for document in cited_documents:
+                cited_document_titles[document['title']] = document['url']
+
+            # print(f"\n{'-'*100}\n")
+                
+        if event.event_type == 'stream-end':
+            # content = event.response.text
+            # if cited_document_titles.keys():
+            #     content += '*Sources*'
+            #     for key in cited_document_titles.keys():
+            #         content = content + key + ": " + cited_document_titles[key]
+            #         # st.markdown(key + ": " + cited_document_titles[key])
+            with st.chat_message("assistant"):
+                # st.markdown(content)
+                st.markdown(event.response.text)
+                if cited_document_titles.keys():
+                    st.markdown("*Sources*")
+                    for key in cited_document_titles.keys():
+                        st.markdown(key + ": " + cited_document_titles[key])
+                st.session_state.messages.append({"role": "assistant", "content": event.response.text})
+
+
